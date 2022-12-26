e66f868d3f9579aaf8e62450d9f5a6a1e2dcf2ff
Ricky <ricardo.vadala@pressganey.com>
2022-12-26 11:06:22 -0300

    re-structuring

--- src/utils/constants/address.ts
+++
@@ -1,1 +0,0 @@
-export const address: string = '0x5FA0C27820e48e62248A7bA5c8960c86DE01373E';
--- src/utils/constants/index.tsx
+++
@@ -1,9 +0,0 @@
-import {abi} from './abi'
-import {address} from './address'
-import {contract} from './contract'
-
-export {
-    abi,
-    address,
-    contract
-}
