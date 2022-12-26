b589f33e3f02a20c50b781e9c9b6f752be1ba998
Ricky <ricardo.vadala@pressganey.com>
2022-12-26 11:40:30 -0300

    compile & deploy

---
+++ loethery-contracts/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'head vocal clock access damage when sugar pumpkin base remember opinion income',
+  'https://goerli.infura.io/v3/e11e3371862f4a53a673c9e5e84b3892'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
